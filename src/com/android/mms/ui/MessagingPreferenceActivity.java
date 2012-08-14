09641d0a30c3b97b51ca45504d98b1fdb90b023b
Jacek Sowiński <mruwek@gmail.com>
2012-08-15 01:47:04 +0200

    Port "Strip Unicode" feature from ICS branch into JB
    All credits goes to Joshua Sheard
    Original Change ID = Ibec615edd371a040a56dd04904218594e851dbd8
    
    Change-Id: I107eff64c20fba0ba024501c86d65d297c6352ff

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -72,6 +72,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String NOTIFICATION_VIBRATE_PATTERN = "pref_key_mms_notification_vibrate_pattern";
     public static final String NOTIFICATION_VIBRATE_PATTERN_CUSTOM = "pref_key_mms_notification_vibrate_pattern_custom";
     public static final String NOTIFICATION_VIBRATE_CALL ="pre_key_mms_notification_vibrate_call";
+    public static final String STRIP_UNICODE            = "pref_key_strip_unicode";
     public static final String FULL_TIMESTAMP            = "pref_key_mms_full_timestamp";
     public static final String SENT_TIMESTAMP            = "pref_key_mms_use_sent_timestamp";
     // Menu entries
