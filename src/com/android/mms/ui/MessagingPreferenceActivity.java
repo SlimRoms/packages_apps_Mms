12daa67df30420c03165e7497fad1e2c7f326c17
DvTonder <david.vantonder@gmail.com>
2013-07-13 09:38:40 -0400

    Mms: Generic Blacklist support (3 of 5)
    Change-Id: Ib809ea56de60b6eace785ffd69158b10c2c9ebbe

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -48,6 +48,7 @@ import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.internal.telephony.util.BlacklistUtils;
 import com.android.mms.MmsApp;
 import com.android.mms.MmsConfig;
 import com.android.mms.R;
@@ -134,6 +135,9 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String USER_AGENT               = "pref_key_mms_user_agent";
     public static final String USER_AGENT_CUSTOM        = "pref_key_mms_user_agent_custom";
 
+    // Blacklist
+    public static final String BLACKLIST                 = "pref_blacklist";
+
     // Menu entries
     private static final int MENU_RESTORE_DEFAULTS       = 1;
 
@@ -182,6 +186,9 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     private CheckBoxPreference mMMSBreath;
     private Preference mTextAreaSize;
 
+    // Blacklist
+    private PreferenceScreen mBlacklist;
+
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -198,9 +205,20 @@ public class MessagingPreferenceActivity extends PreferenceActivity
         super.onResume();
 
         // Since the enabled notifications pref can be changed outside of this activity,
-        // we have to reload it whenever we resume.
+        // we have to reload it whenever we resume, including the blacklist summary
         setEnabledNotificationsPref();
+        updateBlacklistSummary();
         registerListeners();
+    }
+
+    private void updateBlacklistSummary() {
+        if (mBlacklist != null) {
+            if (BlacklistUtils.isBlacklistEnabled(this)) {
+                mBlacklist.setSummary(R.string.blacklist_summary);
+            } else {
+                mBlacklist.setSummary(R.string.blacklist_summary_disabled);
+            }
+        }
     }
 
     private void loadPrefs() {
@@ -258,6 +276,9 @@ public class MessagingPreferenceActivity extends PreferenceActivity
         mInputTypePref = (ListPreference) findPreference(INPUT_TYPE);
         mInputTypeEntries = getResources().getTextArray(R.array.pref_entries_input_type);
         mInputTypeValues = getResources().getTextArray(R.array.pref_values_input_type);
+
+        // Blacklist screen - Needed for setting summary
+        mBlacklist = (PreferenceScreen) findPreference(BLACKLIST);
 
         setMessagePreferences();
     }
