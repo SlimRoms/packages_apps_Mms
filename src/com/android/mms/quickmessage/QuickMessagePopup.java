cfc3e72654e1720cf593577d8f446fc83e4ce6a6
Lars Greiss <kufikugel@googlemail.com>
2013-08-01 18:58:16 +0200

    Fix illegal argument exception
    Change-Id: I36eaaa2bd0677a11efb291861627706389913c0e

--- src/com/android/mms/quickmessage/QuickMessagePopup.java
+++ src/com/android/mms/quickmessage/QuickMessagePopup.java
@@ -304,6 +304,7 @@ public class QuickMessagePopup extends Activity implements
                 QuickMessage qm = new QuickMessage(extras.getString(SMS_FROM_NAME_EXTRA),
                         extras.getString(SMS_FROM_NUMBER_EXTRA), nm);
                 mMessageList.add(qm);
+                mPagerAdapter.notifyDataSetChanged();
 
                 // If triggered from Quick Reply the keyboard should be visible immediately
                 if (extras.getBoolean(QR_SHOW_KEYBOARD_EXTRA, false)) {
@@ -829,6 +830,7 @@ public class QuickMessagePopup extends Activity implements
 
         // Clear the messages list
         mMessageList.clear();
+        mPagerAdapter.notifyDataSetChanged();
 
         if (DEBUG)
             Log.d(LOG_TAG, "clearNotification(): Message list cleared. Size = " + mMessageList.size());
